4a09b76c896038e7d9461b34a9bf9352db7fb646
Netrecov <92816323+Netrecov@users.noreply.github.com>
2026-05-05 06:15:36 -0500

    Create AD_Email_lookup.ps1

---
+++ AD_Email_lookup.ps1
@@ -0,0 +1,37 @@
+Import-Module ActiveDirectory
+
+$results = foreach ($row in Import-Csv "emails.csv") {
+
+    $email = $row.Email
+
+    # Try exact match on mail
+    $user = Get-ADUser -Filter "mail -eq '$email'" -Properties mail, proxyAddresses, userPrincipalName
+
+    # If not found, try proxyAddresses (aliases)
+    if (-not $user) {
+        $user = Get-ADUser -Filter "proxyAddresses -like '*$email*'" -Properties mail, proxyAddresses, userPrincipalName
+    }
+
+    if ($user) {
+        [PSCustomObject]@{
+            Email             = $email
+            Found             = $true
+            Name              = $user.Name
+            SamAccountName    = $user.SamAccountName
+            UserPrincipalName = $user.UserPrincipalName
+            PrimaryEmail      = $user.mail
+        }
+    }
+    else {
+        [PSCustomObject]@{
+            Email             = $email
+            Found             = $false
+            Name              = $null
+            SamAccountName    = $null
+            UserPrincipalName = $null
+            PrimaryEmail      = $null
+        }
+    }
+}
+
+$results | Export-Csv "results.csv" -NoTypeInformation
