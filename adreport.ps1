5e2152d1ef2346f108cd74e161d9500df6ec42f7
Netrecov <92816323+Netrecov@users.noreply.github.com>
2026-08-12 07:55:51 -0500

    Update adreport.ps1

--- adreport.ps1
+++ adreport.ps1
@@ -1,6 +1,6 @@
 Import-Module ActiveDirectory
 
-Get-ADUser -Filter * -Properties GivenName, Surname, Description, Title, TelephoneNumber, Fax, ipPhone |
+Get-ADUser -Filter 'Enabled -eq $true' -Properties GivenName, Surname, Description, Title, TelephoneNumber, Fax, ipPhone |
     Select-Object `
         GivenName,
         Surname,
@@ -9,4 +9,7 @@ Get-ADUser -Filter * -Properties GivenName, Surname, Description, Title, Telepho
         TelephoneNumber,
         Fax,
         ipPhone |
-    Export-Csv -Path "C:\Temp\AD_Users.csv" -NoTypeInformation -Encoding UTF8
+    Tee-Object -Variable Users |
+    Export-Csv -Path "C:\Temp\AD_Enabled_Users.csv" -NoTypeInformation -Encoding UTF8
+
+$Users | Format-Table -AutoSize
